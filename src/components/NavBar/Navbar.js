92bba7490e0e42a04d490a1cce77cfdb62a59447
Leonardo Ramos <40323030+LeonardoRamos-repositorio@users.noreply.github.com>
2023-04-03 18:21:30 -0600

    primer commit

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,17 @@
+const NavBar = ( ) => {
+    return(
+        <nav>
+           <h3>Video Juegos</h3>
+           <div>
+                <button>Catalogos</button>
+                <button>Video Juegos</button>
+                <button>Consolas & Hardware</button>
+                <button>Accesorios de Juego</button>
+                <button>Controles</button>
+                <button>Consolas</button>
+            </div> 
+        </nav>
+    )
+}
+
+export default NavBar
