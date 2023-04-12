8846dd982889aa37729e06478bff00ebff4e8dad
Leonardo Ramos <40323030+LeonardoRamos-repositorio@users.noreply.github.com>
2023-04-11 19:44:16 -0600

    reentrega

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/Navbar';
+import itemListContainer from './components/itemListContainer/itemListContainer.js';
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-      
-    </div>
-  );
+    return (
+        <div className = "App">
+        <NavBar/>
+        <itemListContainer greeting = 'Bienvenidos'/>
+        </div>
+    );
 }
 
 export default App;
